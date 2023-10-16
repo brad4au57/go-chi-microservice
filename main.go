743b8fcd111b4b224ca0e5eb8bf4c4ec498d57e2
Brad Bales <brad4au57@gmail.com>
2023-10-16 15:03:44 -0500

    chi router and middleware logger added

--- main.go
+++ main.go
@@ -3,12 +3,23 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
+	router := chi.NewRouter()
+
+	router.Use(middleware.Logger)
+	router.Get("/", func(w http.ResponseWriter, r *http.Request) { w.Write([]byte("Server is running at port 3000")) })
+	router.Get("/hello", basicHandler)
+
+	// http.ListenAndServe(":3000", router)
+
 	server := &http.Server{
 		Addr:    ":3000",
-		Handler: http.HandlerFunc(basicHandler),
+		Handler: router,
 	}
 
 	err := server.ListenAndServe()
